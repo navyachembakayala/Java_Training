eba2a9781a65299273e7163b7c556325aa923fa4
navyachembakayala <63104660+navyachembakayala@users.noreply.github.com>
2020-04-16 16:19:09 +0530

    Update AccountDetails_Main.java

--- CaseStudy/AccountDetails_Main.java
+++ CaseStudy/AccountDetails_Main.java
@@ -1,19 +1,20 @@
 package CaseStudy;
 
-
-
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
 
 public class AccountDetails_Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		
-		List<Account_Details> lst1 = new ArrayList<Account_Details>();
+		//List<Account_Details> lst1 = new ArrayList<Account_Details>();
+		ArrayList<String> lst = new ArrayList<String>();
 		Scanner s1 = new Scanner(System.in);
 		int j=0;
+		int k = 0;
 		System.out.println("Do you want to create the account: ");
 		String Sr = s1.nextLine();
 		if (Sr.equals("Y")) {
@@ -25,44 +26,66 @@ public class AccountDetails_Main {
 			
 		switch(Sr) {
 		case "Y":
-			Account_Details ac = new Account_Details();
-			System.out.println("Please enter the Account type");
-			String Sr1 = s1.nextLine();
-			ac.setAccount_Type(Sr1);
 			
+			try {
+			Account_Details ac = new Account_Details();
+			System.out.println("Please enter the Options for Account type" + "\n"+" 1 - Savings "+"\n"+" 2 - Current");
+			String sr1 = s1.nextLine();
+			if (sr1.contains("1")) {
+				lst.add("Savings");
+			} else if(sr1.contains("2")) {
+				lst.add("Current");
+			} else {
+				System.out.println("Invalid input" );
+				System.exit(0);
+			}
 			System.out.println("Please enter the user name");
-			String Sr2 = s1.nextLine();
-			ac.setName(Sr2);
+			lst.add(s1.nextLine());
 			
 			System.out.println("Please enter the account number");
-			BigInteger Sr3 = s1.nextBigInteger();
-			ac.setAccount_Number(Sr3);
+			BigInteger accNum= s1.nextBigInteger();
+			s1.nextLine();
+			String s = accNum+"";
+			lst.add(s);		
 			
 			System.out.println("Please enter the account balance");
-			BigDecimal Sr4 = s1.nextBigDecimal();
-			ac.setAmount(Sr4);
+			BigDecimal AccBal= s1.nextBigDecimal();
+			s1.nextLine();
+			String ss = AccBal+"";
+			lst.add(ss);	
 			
 			System.out.println("Please enter the Currency");
-			s1.nextLine();
-			String Sr5 = s1.nextLine();
-			ac.setCurrency(Sr5);
-			lst1.add(ac);			
+			lst.add(s1.nextLine());			
 			continue;
+			}
+			catch(NullPointerException e1) {
+				if (lst.isEmpty()) {
+					System.out.println("Can't handle null arrya");
+				}
+				
+			}
 			
+			catch(Exception e1)
+			{
+				System.out.println("An error occure please give valid input");
+			}
 		case "N":
 			System.exit(0);
 			
+		}		
 		}
+		int n=1;
 		
-		
-		}
-		for(Account_Details ac: lst1 ) {
-			System.out.println("Account Type: "+ ac.Account_Type 
-				    + "\n"+"User Name: "   + ac.getName()
-					+ "\n"+"Account Number: "+ ac.getAccount_Number() 
-					+ "\n"+"Balance: "      + ac.getAmount()
-					+ "\n"+"Currency: "     + ac.Currency);
-		}
+			for( int a=0; a<=lst.size()-1;a+=5) {
+				if(n<=j){
+				System.out.println("Account Type: "+ lst.get(a)+"\n"
+				                  +"User Name...." + lst.get(a+1) 
+							+"\n"+"Account Number...."+lst.get(a+2)+"\n"+
+								 "Account Balance...."+lst.get(a+3)+"\n"+
+					        		"Currency...."+lst.get(a+4)+"\n");
+			}
+				
+			}
 	}
 
 }
